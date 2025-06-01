16faf213a92611158bc8b76f6ba7c0a095a2c960
Lawrence S <lawrence.dev25@gmail.com>
2025-06-01 23:30:46 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Header from "@/pages/header";
-import HeroSection from "@/pages/heroSection";
-import AboutMe from "@/pages/aboutMe";
-import Project from "@/pages/project";
-import Contact from "@/pages/contact";
+import Header from "../pages/header";
+import HeroSection from "../pages/heroSection";
+import AboutMe from "../pages/aboutMe";
+import Project from "../pages/project";
+import Contact from "../pages/contact";
 
 export default function Home() {
   return (
