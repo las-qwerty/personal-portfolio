817434b0ff0c178861ff1c86f89528f89e0078e2
Lawrence S <lawrence.dev25@gmail.com>
2025-06-01 23:38:36 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Header from "../pages/header";
-import HeroSection from "../pages/heroSection";
-import AboutMe from "../pages/aboutMe";
-import Project from "../pages/project";
-import Contact from "../pages/contact";
+import Header from "../components/header";
+import HeroSection from "../components/heroSection";
+import AboutMe from "../components/aboutMe";
+import Project from "../components/project";
+import Contact from "../components/contact";
 
 export default function Home() {
   return (
